f4b6a6dec5b58a1b885ce3827ea9c6ff845f4a9c
someone <someone@someplace.com>
2022-05-10 20:15:13 +1000

    upd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from pyowm import OWM
+
 from datetime import datetime
 from aiogram import Bot, Dispatcher, executor, types
 import markups as nav
@@ -25,23 +26,40 @@ async def command_start(message: types.Message):
 async def command_start(message: types.Message):
     await message.answer('Я в вас верю!!'.format(message.from_user),reply_markup= nav.mainMenu)
 
+
 @dp.message_handler()
 async def bot_message(message:types.Message):
     if message.text == '🎱Рандомное число':
         await bot.send_message(message.chat.id,'Ваше число: '+ str(random.randint(1,101)))
-
-    elif message.text == '🚿Стирка':
-        await bot.send_message(message.chat.id,'Здесь будет запись на стирку')
 
     elif message.text == '👻Мемы':
         await bot.send_message(message.chat.id,'мемы!')
 
     elif message.text == '💵Курсы валют':
         rates = (ExchangeRates(datetime.now()))
-        await bot.send_message(message.chat.id,'💵Курс доллара: ' + str(rates['USD'].rate) +' руб.' + '\n' + '💶Курс евро: ' + str(rates['EUR'].rate) +' руб.')
+        await bot.send_message(message.chat.id,'💵Курс доллара: ' + str(rates['USD'].rate) +' руб.' + '\n' + '💶Курс евро: ' +
+                               str(rates['EUR'].rate) +' руб.')
 
     elif message.text == '☔️Погода':
-        await bot.send_message(message.chat.id,'Погода Хабаровск: ' + w.detailed_status + str(w.temperature('celsius')['temp_max']))
+        await bot.send_message(message.chat.id,'🌤Погода в Хабаровске: '+ '\n' + w.detailed_status + '\n' + 'Температура: '+
+                               str(round(w.temperature('celsius')['temp'])) + '°' + '\n' + 'Скорость ветра: ' + str(round(w.wind()['speed'])) +
+                               ' м.с')
+
+
+    elif message.text == '🚿Стирка':
+        await bot.send_message(message.chat.id, 'Выберите время для стирки',reply_markup=nav.inline_kb1)
+
+        async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
+            time = []  # 0-9 считается по плашкам на сайте
+            machine = []  # 0-3
+            req = f'http://xromen.pythonanywhere.com/done/?interval={time}&aNum={machine}&oom=438&secName=Орлов]'
+            code = callback_query.data[-1]
+            if code.isdigit():
+                time=code
+                await bot.send_message(message.chat.id, '[f[f')
+
+
+
 
 
 
--- markups.py
+++ markups.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 # Main Menu
 btnRandom = KeyboardButton('🎱Рандомное число')
@@ -7,3 +7,21 @@ btnWash= KeyboardButton('🚿Стирка')
 btnMoney = KeyboardButton('💵Курсы валют')
 btnWeather = KeyboardButton('☔️Погода')
 mainMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnRandom,btnMemes,btnWash,btnMoney, btnWeather)
+
+inline_btn_0 = InlineKeyboardButton('8:00-9:30', callback_data='btn0')
+inline_btn_1 = InlineKeyboardButton('9:30-11:00', callback_data='btn1')
+inline_btn_2 = InlineKeyboardButton('11:00-12:30', callback_data='btn2')
+inline_btn_3 = InlineKeyboardButton('12:30-14:00', callback_data='btn3')
+inline_btn_4 = InlineKeyboardButton('14:00-15:30', callback_data='btn4')
+inline_btn_5 = InlineKeyboardButton('15:30-17:00', callback_data='btn5')
+inline_btn_6 = InlineKeyboardButton('17:00-18:30', callback_data='btn6')
+inline_btn_7 = InlineKeyboardButton('18:30-20:00', callback_data='btn7')
+inline_btn_8 = InlineKeyboardButton('20:00-21:30', callback_data='btn8')
+inline_btn_9 = InlineKeyboardButton('20:00-21:30', callback_data='btn9')
+
+inline_kb1 = InlineKeyboardMarkup(row_width=2)
+inline_kb1.add(inline_btn_0, inline_btn_1)
+inline_kb1.add(inline_btn_2, inline_btn_3)
+inline_kb1.add(inline_btn_4, inline_btn_5)
+inline_kb1.add(inline_btn_6, inline_btn_7)
+inline_kb1.add(inline_btn_8, inline_btn_9)
