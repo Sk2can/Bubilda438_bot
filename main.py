1a0fcec50a7fafcaec4caea2feacbcd10b04bbcb
Vladislav Khmelev <khmelev.vladislav@gmail.com>
2023-02-23 10:04:18 +1000

    Перезалив репозитория

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 from pyowm import OWM
 from datetime import datetime
 from aiogram import Bot, Dispatcher, executor, types
@@ -11,57 +14,54 @@ mgr = owm.weather_manager()
 observation = mgr.weather_at_place('Khabarovsk,RU')
 w = observation.weather
 
+mouths = {1: 'января', 2: 'февраля', 3: 'марта', 4: 'апреля', 5: 'мая', 6: 'июня', 7: 'июля', 8: 'августа', 9: 'сентября', 10: 'октября', 11: 'ноября', 12: 'декабря'}
+
 TOKEN = '5116309199:AAHnaiV0dkA_CXcFy_59ScGFc8KWpdsUzVc'
 
-bot= Bot(token=TOKEN)
-dp= Dispatcher(bot)
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-@dp.message_handler (commands=['start'])
+@dp.message_handler(commands=['start'])
 async def command_start(message: types.Message):
-    await message.answer('🔸 Бот готов к использованию.\n🔸 Если не появились вспомогательные кнопки \n ▶️ Введите /start'.format(message.from_user),reply_markup= nav.mainMenu)
+    await message.answer(
+        '🔸 Бот готов к использованию.\n🔸 Если не появились вспомогательные кнопки \n ▶️ Введите /start'.format(
+            message.from_user), reply_markup=nav.mainMenu)
 
-@dp.message_handler (commands=['help'])
+
+@dp.message_handler(commands=['help'])
 async def command_start(message: types.Message):
-    await message.answer('При возникновении проблем обращайтесь к https://t.me/SK_2can'.format(message.from_user),reply_markup= nav.mainMenu)
+    await message.answer('При возникновении проблем обращайтесь к https://t.me/SK_2can'.format(message.from_user),
+                         reply_markup=nav.mainMenu)
 
 
 @dp.message_handler()
-async def bot_message(message:types.Message):
+async def bot_message(message: types.Message):
     if message.text == '🎱Рандомное число':
-        await bot.send_message(message.chat.id,'Ваше число: '+ f'<tg-spoiler> {str(random.randint(1,101))} </tg-spoiler>', parse_mode=types.ParseMode.HTML)
-
-    elif message.text == '👻Мемы':
-        await bot.send_message(message.chat.id,'Troll face')
+        await bot.send_message(message.chat.id,
+                               'Ваше число: ' + f'<tg-spoiler> {str(random.randint(1, 101))} </tg-spoiler>',
+                               parse_mode=types.ParseMode.HTML)
 
     elif message.text == '💵Курсы валют':
         rates = (ExchangeRates(datetime.now()))
-        await bot.send_message(message.chat.id,'💵Курс доллара: ' + str(rates['USD'].rate) +' руб.' + '\n' + '💶Курс евро: ' +
-                               str(rates['EUR'].rate) +' руб.')
+        await bot.send_message(message.chat.id,
+                               'Курс валют на ' + str(rates.date_received.day) + ' ' + mouths[rates.date_received.month] + ':' + '\n' '💵Курс доллара: ' + str(rates['USD'].rate) + ' руб.' + '\n' + '💶Курс евро: ' +
+                               str(rates['EUR'].rate) + ' руб.')
 
     elif message.text == '☔️Погода':
-        await bot.send_message(message.chat.id,'🌤Погода в Хабаровске: '+ '\n' + w.detailed_status + '\n' + 'Температура: '+
-                               str(round(w.temperature('celsius')['temp'])) + '°' + '\n' + 'Скорость ветра: ' + str(round(w.wind()['speed'])) +
+        await bot.send_message(message.chat.id,
+                               '🌤Погода в Хабаровске: ' + '\n' + w.detailed_status + '\n' + 'Температура: ' +
+                               str(round(w.temperature('celsius')['temp'])) + '°' + '\n' + 'Скорость ветра: ' + str(
+                                   round(w.wind()['speed'])) +
                                ' м.с')
 
-    elif message.text == '🚿Стирка':
-        await bot.send_message(message.chat.id, 'Выберите время для стирки',reply_markup=nav.inline_kb1)
-        async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
-            time = []  # 0-9 считается по плашкам на сайте
-            machine = []  # 0-3
-            req = f'http://xromen.pythonanywhere.com/done/?interval={time}&aNum={machine}&oom=438&secName=Орлов]'
-            code = callback_query.data[-1]
-            if code.isdigit():
-                time=code
-                await bot.send_message(message.chat.id, '[f[f')
 
 @dp.callback_query_handler(text_contains='btn0')
-async def  wash(call: types.CallbackQuery):
+async def wash(call: types.CallbackQuery):
     await call.answer(cache_time=60)
     callback_data = call.data
     logging.info(f"call = {callback_data}")
-    await  call.message.answer('Выберите машинку', reply_markup=nav.inline_kb2)
-
+    await call.message.answer('Выберите машинку', reply_markup=nav.inline_kb2)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates = False)
+    executor.start_polling(dp, skip_updates=False)
